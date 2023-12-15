eae5bbad84807025a4c25cd961f5a6eea2d84a40
Emre Gültekin <emre.gltkn24@gmail.com>
2023-12-15 16:32:39 +0300

    -

--- weatherapp.py
+++ weatherapp.py
@@ -1,12 +1,12 @@
 """Module providing a function printing python version."""
 import requests
 
-api_key = '45227d46ee50ed501779eeab26c62bf7'
+API_KEY = '45227d46ee50ed501779eeab26c62bf7'
 
 user_input = input('Enter City: ')
 
 weather_data = requests.get(
-    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}"
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={API_KEY}"
     ,timeout=5)
 
 weather = weather_data.json()['weather'][0]['main']
